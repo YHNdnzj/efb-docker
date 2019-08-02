624c45ded68acd537b3e50078e072799fc7e880d
Mike Yuan <me@yhndnzj.com>
2019-08-02 18:33:03 +0800

    Use Chinese

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 if [ "$EFB_PROFILE" ]; then
-	ehforwarderbot -p "$EFB_PROFILE"
+	LANGUAGE=zh_CN.UTF-8 LC_ALL=zh_CN.UTF-8 LC_MESSAGES=zh_CN.UTF-8 LANG=zh_CN.UTF-8 python3 -m ehforwarderbot -p "$EFB_PROFILE"
 else
 	echo "請使用 -e EFB_PROFILE=wechat/qq 選擇要啟動的 Profile"
 fi
