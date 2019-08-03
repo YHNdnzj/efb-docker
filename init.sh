b721cc65ec2bedd7c380da86a265dfdbc1ddee4b
Mike Yuan <me@yhndnzj.com>
2019-08-03 20:32:57 +0800

    Use cat instead of echo

--- init.sh
+++ init.sh
@@ -1,10 +1,29 @@
 #!/bin/bash
 ETM_CONFIG='token: "TOKEN"\nadmins: \n- ID'
 mkdir -p /etc/ehforwarderbot/profiles
-cd /etc/ehforwarderbot/profiles || exit
+cd /etc/ehforwarderbot/profiles || exit 1
 mkdir -p wechat/blueset.telegram qq/{blueset.telegram,milkice.qq/coolq}
-echo -e "master_channel: blueset.telegram\nslave_channels: \n- blueset.wechat" > wechat/config.yaml
-echo -e "master_channel: blueset.telegram\nslave_channels: \n- milkice.qq" > qq/config.yaml
+cat << EOF > wechat/config.yaml
+master_channel: blueset.telegram
+slave_channels: 
+- blueset.wechat
+EOF
+cat << EOF > qq/config.yaml
+master_channel: blueset.telegram
+slave_channels: 
+- milkice.qq
+EOF
 echo -e "$ETM_CONFIG" > wechat/blueset.telegram/config.yaml
 echo -e "$ETM_CONFIG" > qq/blueset.telegram/config.yaml
-echo -e "Client: CoolQ\nCoolQ:\n    type: HTTP\n    access_token: ac0f790e1fb74ebcaf45da77a6f9de47\n    api_root: http://cqhttp:5700/\n    host: 0.0.0.0\n    port: 8000\n    is_pro: false\n    air_option:\n        upload_to_smms: true" > qq/milkice.qq/config.yaml
+cat << EOF > qq/milkice.qq/config.yaml
+Client: CoolQ
+CoolQ:
+    type: HTTP
+    access_token: ac0f790e1fb74ebcaf45da77a6f9de47
+    api_root: http://cqhttp:5700/
+    host: 0.0.0.0
+    port: 8000
+    is_pro: false
+    air_option:
+        upload_to_smms: true
+EOF
