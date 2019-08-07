bbe4626f3ffc0c512b3c1330b32218651278dc7a
Mike Yuan <me@yhndnzj.com>
2019-08-07 11:07:49 +0800

    FROM python:3

--- Dockerfile
+++ Dockerfile
@@ -1,14 +1,10 @@
-FROM debian:buster-slim
+FROM python:3
 MAINTAINER yhndnzj
 
 RUN apt-get update \
 	&& apt-get install -y \
-		python3-pip \
-		python3-wheel \
 		ffmpeg \
-		libmagic1 \
-		libwebp6 \
-	&& python3 -m pip install \
+	&& pip install \
 		ehforwarderbot \
 		efb-telegram-master \
 		efb-wechat-slave \
--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
-#!/bin/sh
+#!/bin/bash
 if [ "$EFB_PROFILE" ]; then
-	LANGUAGE=zh_CN.UTF-8 LC_ALL=zh_CN.UTF-8 LC_MESSAGES=zh_CN.UTF-8 LANG=zh_CN.UTF-8 python3 -m ehforwarderbot -p "$EFB_PROFILE"
+	env LANGUAGE=zh_CN.UTF-8 LC_ALL=zh_CN.UTF-8 LC_MESSAGES=zh_CN.UTF-8 LANG=zh_CN.UTF-8 ehforwarderbot -p "$EFB_PROFILE"
 else
 	echo "請使用 -e EFB_PROFILE=wechat/qq 選擇要啟動的 Profile"
 fi
