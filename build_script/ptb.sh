8429eb2e2b052faae4439931d5cf89df102e1449
Mike Yuan <me@yhndnzj.com>
2019-08-15 22:51:27 +0800

    Fix a bug

--- build_script/ptb.sh
+++ build_script/ptb.sh
@@ -6,8 +6,7 @@ cd python-telegram-bot-e247fa7c2c254ec25e1cac390217f06f5f5c51f4 || exit 1
 cd telegram/vendor || exit 1
 wget -qO urllib3.zip  https://github.com/python-telegram-bot/urllib3/archive/d2403a79fc38afbdd9aba8a05d274a83dc8bb412.zip
 unzip -q urllib3.zip
-rm urllib3.zip
-rmdir ptb_urllib3
+rm -rf urllib3.zip ptb_urllib3
 mv urllib3-d2403a79fc38afbdd9aba8a05d274a83dc8bb412 ptb_urllib3
 )
 python3 setup.py install
