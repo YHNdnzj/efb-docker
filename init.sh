bc1f26426c519f573b30ebf12dbb75e6ecb8b764
Mike Yuan <me@yhndnzj.com>
2020-03-06 10:29:47 +0800

    Fix a bug & add filter middleware

--- init.sh
+++ init.sh
@@ -1,18 +1,24 @@
 #!/bin/bash
 docker pull yhndnzj/efb
 
-mkdir -p /etc/ehforwarderbot/profiles
-cd /etc/ehforwarderbot/profiles || exit 1
-mkdir -p wechat/blueset.telegram qq/{blueset.telegram,milkice.qq/coolq}
+mkdir -p /etc/ehforwarderbot/{profiles,modules}
 
-cat <<EOF >{wechat,qq}/config.yaml
+(
+    cd /etc/ehforwarderbot/profiles || exit 1
+    mkdir -p wechat/blueset.telegram qq/{blueset.telegram,milkice.qq/coolq}
+
+    cat <<EOF >{wechat,qq}/config.yaml
 master_channel: blueset.telegram
 slave_channels: 
 EOF
-echo "- blueset.wechat" > wechat/config.yaml
-echo "- milkice.qq" > qq/config.yaml
-echo -e "token: \"TOKEN\"\nadmins: \n- ID" >{wechat,qq}/blueset.telegram/config.yaml
-cat <<EOF > qq/milkice.qq/config.yaml
+    echo "- blueset.wechat" >> wechat/config.yaml
+    echo "- milkice.qq" >> qq/config.yaml
+    cat <<EOF >>{wechat,qq}/config.yaml
+middlewares: 
+- filter.FilterMiddleware
+EOF
+    echo -e "token: \"TOKEN\"\nadmins: \n- ID" >{wechat,qq}/blueset.telegram/config.yaml
+    cat <<EOF > qq/milkice.qq/config.yaml
 Client: CoolQ
 CoolQ:
     type: HTTP
@@ -24,3 +30,6 @@ CoolQ:
     air_option:
         upload_to_smms: true
 EOF
+)
+
+wget -qP /etc/ehforwarderbot/modules https://gist.github.com/blueset/0084ab142e2213bca50d81d08753e564/raw/8699f4b7007b396ff06493eb3ded34402b19d5d0/filter.py
