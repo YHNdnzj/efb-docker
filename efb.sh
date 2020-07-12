e6accd3eca26033d16cd0e69bc984e2f2e932aa6
Mike Yuan <me@yhndnzj.com>
2020-07-12 17:23:28 +0800

    Output error message to stderr

--- efb.sh
+++ efb.sh
@@ -2,5 +2,6 @@
 if [[ $EFB_PROFILE ]]; then
     LANG=zh_CN.UTF-8 exec ehforwarderbot -p "$EFB_PROFILE"
 else
-    echo "請使用 -e EFB_PROFILE=wechat/qq 選擇要啟動的 Profile"
+    echo "請使用 -e EFB_PROFILE=wechat/qq 選擇要啟動的 Profile" >&2
+    exit 1
 fi
